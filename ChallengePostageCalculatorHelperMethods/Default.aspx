280e0a685305d77cc9859a9dac22964bdba3f777
Tim Denison <tim.denison@gmail.com>
2015-07-16 12:39:23 -0700

    Simple Postage Calculator Web App

---
+++ ChallengePostageCalculatorHelperMethods/Default.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengePostageCalculatorHelperMethods.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        Postal Calculator<br />
+        <br />
+    
+    </div>
+        Width:&nbsp;
+        <asp:TextBox ID="widthInput" runat="server" OnTextChanged="text_TextChanged" AutoPostBack="True"></asp:TextBox>
+        <br />
+        Height:&nbsp; <asp:TextBox ID="heightInput" runat="server" OnTextChanged="text_TextChanged" AutoPostBack="True" TabIndex="1"></asp:TextBox>
+        <br />
+        Length:
+        <asp:TextBox ID="lengthInput" runat="server" OnTextChanged="text_TextChanged" AutoPostBack="True" TabIndex="2"></asp:TextBox>
+        <br />
+        <br />
+        <asp:RadioButton ID="shippingGroundRadioButton" runat="server" AutoPostBack="True" GroupName="ShippingType" OnCheckedChanged="radioButton_CheckedChanged" Text="Ground" />
+        <br />
+        <asp:RadioButton ID="shippingAirRadioButton" runat="server" AutoPostBack="True" GroupName="ShippingType" OnCheckedChanged="radioButton_CheckedChanged" Text="Air" />
+        <br />
+        <asp:RadioButton ID="shippingNextDayRadioButton" runat="server" AutoPostBack="True" GroupName="ShippingType" OnCheckedChanged="radioButton_CheckedChanged" Text="Next Day" />
+        <br />
+        <br />
+        <asp:Label ID="result" runat="server"></asp:Label>
+    </form>
+    <p>
+        &nbsp;</p>
+</body>
+</html>
---
+++ ChallengePostageCalculatorHelperMethods/Default.aspx.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Windows.Forms;
+
+
+
+namespace ChallengePostageCalculatorHelperMethods
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //if (!Page.IsPostBack)
+            //{
+            //   main();
+            //}
+        }
+
+       public void main(string textBoxID)
+        {
+           switch (textBoxID)
+           {
+               case "widthInput":
+                   textBoxID = "heightInput";
+                   break;
+               case "heightInput":
+                   textBoxID = "lengthInput";
+                   break;
+               case "lengthInput":
+                   textBoxID = "widthInput";
+                   break;
+           }
+           this.FindControl(textBoxID).Focus();
+
+            if (buttonIsChecked())
+            {
+                
+                if (validRequest())
+                {
+                    int w = (string.IsNullOrWhiteSpace(widthInput.Text)) ? 1 : Convert.ToInt32(widthInput.Text);
+                    int h = (string.IsNullOrWhiteSpace(heightInput.Text)) ? 1 : Convert.ToInt32(heightInput.Text);
+                    int l = (string.IsNullOrWhiteSpace(lengthInput.Text)) ? 1 : Convert.ToInt32(lengthInput.Text);
+                    int volume = calcVolume(w, h, l);
+
+
+
+                    if (shippingGroundRadioButton.Checked)
+                    {
+                        result.Text = String.Format("Your parcel will cost {0:C} to ship.", (volume * .15));
+                    }
+                    else if (shippingAirRadioButton.Checked)
+                    {
+                        result.Text = String.Format("Your parcel will cost {0:C} to ship.", (volume * .25));
+                    }
+                    else if (shippingNextDayRadioButton.Checked)
+                    {
+                        result.Text = String.Format("Your parcel will cost {0:C} to ship.", (volume * .45));
+                    }
+
+
+                }
+                else
+                {
+                    MessageBox.Show("Please enter at least two measurements.", "Try again, Chump.", MessageBoxButtons.OK);
+                }
+            }
+
+            }
+       public void main()
+       {
+           
+           if (buttonIsChecked())
+           {
+
+               if (validRequest())
+               {
+                   int w = (string.IsNullOrWhiteSpace(widthInput.Text)) ? 1 : Convert.ToInt32(widthInput.Text);
+                   int h = (string.IsNullOrWhiteSpace(heightInput.Text)) ? 1 : Convert.ToInt32(heightInput.Text);
+                   int l = (string.IsNullOrWhiteSpace(lengthInput.Text)) ? 1 : Convert.ToInt32(lengthInput.Text);
+                   int volume = calcVolume(w, h, l);
+
+
+
+                   if (shippingGroundRadioButton.Checked)
+                   {
+                       result.Text = String.Format("Your parcel will cost {0:C} to ship.", (volume * .15));
+                   }
+                   else if (shippingAirRadioButton.Checked)
+                   {
+                       result.Text = String.Format("Your parcel will cost {0:C} to ship.", (volume * .25));
+                   }
+                   else if (shippingNextDayRadioButton.Checked)
+                   {
+                       result.Text = String.Format("Your parcel will cost {0:C} to ship.", (volume * .45));
+                   }
+
+
+               }
+               else
+               {
+                   MessageBox.Show("Please enter at least two measurements.", "Try again, Chump.", MessageBoxButtons.OK);
+               }
+           }
+
+       }
+
+       public int calcVolume(int width, int height, int length)
+        {
+            int volume = width * height * length;
+            return (volume);
+        }
+
+       public bool validRequest()
+       {
+           if (buttonIsChecked())
+           {
+               int fieldCount = 0;
+               fieldCount = (string.IsNullOrWhiteSpace(widthInput.Text)) ? fieldCount : fieldCount + 1;
+               fieldCount = (string.IsNullOrWhiteSpace(heightInput.Text)) ? fieldCount : fieldCount + 1;
+               fieldCount = (string.IsNullOrWhiteSpace(lengthInput.Text)) ? fieldCount : fieldCount + 1;
+
+               if (fieldCount >= 2)
+               {
+                   return true;
+               }
+               else
+               {
+                   return false;
+               }
+           }
+           else
+           {
+               return false;
+           }
+       }
+
+       protected void text_TextChanged(object sender, EventArgs e)
+       {
+           
+           System.Web.UI.WebControls.TextBox currentTextBox = (System.Web.UI.WebControls.TextBox)sender;
+           string idString = currentTextBox.ID;
+           //var nextElement = currentElement.NextSibling;
+           main(idString);
+       }
+
+     
+       protected void radioButton_CheckedChanged(object sender, EventArgs e)
+       {
+           main();
+       }
+        
+        protected bool buttonIsChecked()
+       {
+            if (shippingGroundRadioButton.Checked || shippingAirRadioButton.Checked || shippingNextDayRadioButton.Checked)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+       }
+    }
+}
